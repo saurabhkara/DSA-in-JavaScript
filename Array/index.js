715b67149be17d319fec5a44dd4eb1e27dd41994
Saurabh <saurabkumar679@gmail..com>
2024-03-29 08:13:23 +0530

    array started

---
+++ Array/index.js
@@ -0,0 +1,13 @@
+console.log("Ram Ram");
+
+// Q.1 How do you check if an element exist?
+
+// Q.2 Check element exist and return array index
+
+// Q.3 How to add, delete and update element from specific index.
+
+// Q.4 Difference between Slice and Splice method
+
+// Q.5 Shallow copy and Deep Copy
+
+// Q.6 Make Deep copy of array
