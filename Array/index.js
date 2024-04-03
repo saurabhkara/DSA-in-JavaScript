aee6a9043a141f605e37681792e45e97d557949f
Saurabh <saurabkumar679@gmail..com>
2024-04-03 16:08:20 +0530

    string questions

--- Array/index.js
+++ Array/index.js
@@ -93,8 +93,121 @@ const res = arr4.slice(1, 4);
 let arr5 = [12, 45, 78, 98];
 let tempArr = arr5; //Shallow copy;
 tempArr[0] = 587;
-console.log(arr5);
 
 let deepArr = [...arr5];
 deepArr[1] = 879;
-console.log(arr5);
+// console.log(arr5);
+
+// Q.7 concatenate two arrays
+
+// inbuild method conact
+const arr6 = [5, 6, 7, 8, 9];
+const arr7 = [9, 8, 6, 3, 6];
+const result7 = arr7.concat(arr6);
+console.log(result7);
+
+// custom function
+function concatArray(arr1, arr2) {
+  let result = [];
+  for (let val of arr1) {
+    result.push(val);
+  }
+
+  for (let val of arr2) {
+    result.push(val);
+  }
+  console.log(result);
+}
+
+concatArray(arr6, arr7);
+
+// Q.8 Check two arrays are equal
+
+let arr8 = [5, 8, 9, 7, 3, 5];
+let arr9 = [5, 8, 9, 7, 3, 5];
+
+function isArrayEqual(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// console.log(isArrayEqual(arr8, arr9));
+// console.log(isArrayEqual(arr8, arr7));
+
+// Q.9 Sort array in Ascending and Descending order
+
+const arr10 = [5, 8, 9, 4, 6, 7];
+arr10.sort((a, b) => a - b);
+// console.log(arr10);
+arr10.sort((a, b) => b - a);
+// console.log(arr10);
+
+// Q.10 Reverse an array
+const arr11 = [5, 8, 9, 3, 0, 4, 6];
+const arr12 = [5, 8, 9, 3, 0, 4, 6];
+//inbuilt method reverse()
+arr11.reverse();
+console.log(arr11);
+
+function arrReverse(arr) {
+  let len = arr.length;
+  for (let i = 0; i < parseInt(len / 2); i++) {
+    let temp = arr[i];
+    arr[i] = arr[len - i - 1];
+    arr[len - i - 1] = temp;
+  }
+  console.log(arr);
+}
+
+// arrReverse(arr12);
+
+// Q.11 map, filter, find,every and reduce methods
+
+let arr13 = [5, 4, 6, 8, 3, 2, 1];
+
+const mappedArr = arr13.map((item, index) => item + index);
+
+const filteredArr = arr13.filter((item, index) => index % 2);
+
+const findArr = arr13.find((item) => item === 2);
+
+const reducedResult = arr13.reduce((acc, item) => acc + item, 0);
+
+// console.log(mappedArr);
+// console.log(filteredArr);
+// console.log(findArr);
+// console.log(reducedResult);
+
+// Q.12 Flattening the array
+
+let arr14 = [1, [2, 3, [4, 5, 6]]];
+
+// const flattendArr = arr14.flat(2);
+// console.log(flattendArr);
+
+let flattendResultArr = [];
+
+function flatArr(arr) {
+  if (!Array.isArray(arr)) {
+    return;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flatArr(arr[i]);
+    } else {
+      flattendResultArr.push(arr[i]);
+    }
+  }
+}
+
+flatArr(arr14);
+console.log(flattendResultArr);
---
+++ String/index.js
@@ -0,0 +1,194 @@
+console.log("Radhe Radhe");
+
+// Q.1  charAt()
+
+let str1 = "Saurabh";
+console.log(str1.charAt(2));
+
+// Q.2 charCodeAt()
+
+console.log("A".charCodeAt());
+
+// Q.3  includes()
+
+console.log(str1.includes("a"));
+
+// Q.4 indexOf() and lastIndexOf()
+
+console.log(str1.indexOf("S"));
+console.log(str1.lastIndexOf("a"));
+
+// Q.5 localeCompare
+
+let str2 = "Saurabhi";
+
+console.log(str1.localeCompare(str2)); // if equals returns 0 else -1
+
+// Q.6 substrings() and slice()
+// Difference in both is slice can take negative index too
+console.log(str1.substring(2));
+console.log(str1.substring(2, 6));
+console.log(str1.slice(2, 6));
+console.log(str1.slice(-4, -1));
+
+// Q.7 trim(), trimEnd()  and trimStart()
+
+let str3 = "    Saurabh is good JavaScript developer      ";
+console.log(str3.trim());
+console.log(str3.trimEnd());
+console.log(str3.trimStart());
+
+// Q.8 toUpperCase() , toLowerCase()
+
+console.log(str1.toLowerCase());
+console.log(str1.toUpperCase());
+
+// Q.9 Stringify the object
+
+const obj = {
+  name: "Saurabh",
+};
+
+console.log(obj);
+console.log(JSON.stringify(obj));
+
+// Q.10 replace() and replaceAll()
+
+const str4 = "I Saurabh is best react native developer. Yes, Saurabh";
+console.log(str4.replace("Saurabh", "Saurya"));
+console.log(str4.replaceAll("Saurabh", "Saurya"));
+
+// Q.11 Find the index of first occurance in string
+
+function returnFirstOccuranceIndex(str, searchStr) {
+  let len1 = str.length;
+  let len2 = searchStr.length;
+
+  for (let i = 0; i < len1 - len2 + 1; i++) {
+    let subs = str.substring(i, i + len2);
+    if (subs === searchStr) {
+      return i;
+    }
+  }
+  return -1;
+}
+console.log(returnFirstOccuranceIndex(str4, "Saurabh"));
+
+// Q.12 Reverse the string
+
+function reverseString(str) {
+  let len = str.length - 1;
+  let result = "";
+  while (len >= 0) {
+    result = result + str[len];
+    len--;
+  }
+  return result;
+}
+// console.log(reverseString("saurabh"));
+// console.log(reverseString("RadhaRani"));
+
+// Q.13 Check given two strings are Anagram
+
+let str5 = "RAMARAMAA";
+let str6 = "RAMAMARA";
+
+function checkAnagram(str1, str2) {
+  let len1 = str1.length;
+  let len2 = str2.length;
+  if (len1 !== len2) {
+    return false;
+  }
+  let obj = {};
+
+  for (let char of str1) {
+    obj[char] = obj[char] ? obj[char] + 1 : 1;
+  }
+  for (let char of str2) {
+    if (obj[char]) {
+      obj[char] = obj[char] - 1;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+// console.log(checkAnagram(str5, str6));
+
+// Q.14 Longest common prefix in array of string
+
+const arr7 = ["RamaRama", "Rama", "Ram"];
+
+function commonPrefixUntil(str1, str2) {
+  let len1 = str1.length;
+  let len2 = str2.length;
+  let prefix = "";
+  for (let i = 0, j = 0; i < len1 && j < len2; i++, j++) {
+    if (str1[i] !== str2[j]) {
+      break;
+    }
+    prefix = prefix + str1[i];
+  }
+  return prefix;
+}
+
+function commonPrefix(arr) {
+  let result = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    result = commonPrefixUntil(arr[i], result);
+  }
+  return result;
+}
+
+// console.log(commonPrefix(arr7));
+
+// Q.15 Merge the strings by adding alternative letter.
+
+let str7 = "Ram";
+let str8 = "Sita";
+
+function mergeAlternate(str1, str2) {
+  let len1 = str1.length;
+  let len2 = str2.length;
+  let result = "";
+  let i = 0;
+  let j = 0;
+  while (i < len1 || j < len2) {
+    if (i < len1) {
+      result = result + str1[i];
+      i++;
+    }
+    if (j < len2) {
+      result = result + str2[j];
+      j++;
+    }
+  }
+  return result;
+}
+console.log(mergeAlternate(str7, str8));
+
+// Q.16 Lenghth of last word in string
+
+const str9 =
+  "Saurabh is best javascript developer, i can do hell alot of things";
+
+function lastWordLength(str) {
+  let wordArr = str.split(" ");
+  let len = wordArr.length;
+  return wordArr[len - 1].length;
+}
+console.log(lastWordLength(str9));
+
+// Q.17 Valid Palindrome, After converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters.
+
+// Q.18 Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+// If the group's length is 1, append the character to s.
+// Otherwise, append the character followed by the group's length.
+
+// Q.19 Reverse the words of string
+
+// Q.20 Reverse the vowel of string
+
+// Q.21 Rotate the string
