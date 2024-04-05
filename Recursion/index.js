61e3ae36e1390f9d7a913419b4156ecec30ef4a5
Saurabh <saurabkumar679@gmail..com>
2024-04-05 23:22:27 +0530

    added recursion question

--- Recursion/index.js
+++ Recursion/index.js
@@ -13,9 +13,46 @@ console.log(factorial(5));
 
 // Q.2 Sum of elements of given array using recursion
 
+const arr1 = [1, 2, 3, 4, 5, 6];
+
+function sumElement(arr, n) {
+  if (n == 1) {
+    return arr[0];
+  }
+  const lastElem = arr.pop();
+  arr.length = n - 1;
+  return lastElem + sumElement(arr, n - 1);
+}
+
+console.log(sumElement(arr1, 6));
+
 // Q.3 find the product of elements of given array using recursion
 
+const arr2 = [1, 2, 3, 4];
+
+function productOfElements(arr, n) {
+  if (n === 1) {
+    return arr[0];
+  }
+  const lastElem = arr[n - 1];
+  arr.length = n - 1;
+  return lastElem * productOfElements(arr, n - 1);
+}
+
+console.log(productOfElements(arr2, 4));
+
 // Q.4 Fibonacci of given numner using recusion
+
+function fibonacci(num) {
+  if (num === 0 || num === 1) {
+    return num;
+  }
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(4));
 
 // Q.5 Check whether a string is palindrome or not
 
