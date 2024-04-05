99d2f008401348f7757958cafa04335faf9fcb9f
Saurabh <saurabkumar679@gmail..com>
2024-04-05 08:47:13 +0530

    recursion folder created

---
+++ Recursion/index.js
@@ -0,0 +1,32 @@
+console.log("Raja Ram");
+
+// Q.1 Factorial of given number using recusion
+
+function factorial(num) {
+  if (num === 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(5));
+
+// Q.2 Sum of elements of given array using recursion
+
+// Q.3 find the product of elements of given array using recursion
+
+// Q.4 Fibonacci of given numner using recusion
+
+// Q.5 Check whether a string is palindrome or not
+
+// Q.6 Create pow(x, n) function which returns x^n
+
+// Q.7 Create a function which returns the sum of digits of a number (e.g., sumOfDigits(453) is 12)
+
+// Q.8 Create a function which returns the number of digits in a number (e.g., countDigits(453) is 3)
+
+// Q.9 Create a function to find the LCM of two numbers
+
+// Q.10 Create a function to find the GCD of two numbers
+
+// Q,11 Create a function to reverse a string
