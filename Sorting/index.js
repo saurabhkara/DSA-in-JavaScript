a8f4263ab81dbda50343ee5f9fe93c28c6a20d79
Saurabh <saurabkumar679@gmail..com>
2024-05-22 06:36:46 +0530

    insertion sort

--- Sorting/index.js
+++ Sorting/index.js
@@ -46,4 +46,21 @@ console.log(selectionSort([7, 6, 2, 6, 1]));
 
 // Q.3 Insertion Sort
 
-function insertionSort() {}
+function insertionSort(arr) {
+  if (!Array.isArray(arr)) {
+    return;
+  }
+
+  for (let i = 1; i < arr.length; i++) {
+    let j = i - 1;
+    let temp = arr[i];
+    while (j >= 0 && arr[j] > temp) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = temp;
+  }
+  console.log(arr);
+}
+
+console.log(insertionSort([7, 6, 2, 6, 1]));
