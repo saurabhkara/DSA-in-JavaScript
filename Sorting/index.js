b542c6812821e6f33a3f82ada46c3fba7ef121c6
Saurabh <saurabkumar679@gmail..com>
2024-05-25 11:18:01 +0530

    merge sort

--- Sorting/index.js
+++ Sorting/index.js
@@ -123,4 +123,52 @@ console.log(quickSortWithoutExtraMemory([5, 8, 6, 4, 3], 0, 4));
 
 // Merge Sort
 
-function mergeSort(arr) {}
+function mergeSort(arr, low, high) {
+  if (low >= high) {
+    return;
+  }
+  let mid = low + parseInt((high - low) / 2);
+  mergeSort(arr, low, mid);
+  mergeSort(arr, mid + 1, high);
+  mergeElement(arr, low, mid, high);
+}
+
+function mergeElement(arr, l, m, r) {
+  var n1 = m - l + 1;
+  var n2 = r - m;
+  var L = new Array(n1);
+  var R = new Array(n2);
+
+  for (var i = 0; i < n1; i++) L[i] = arr[l + i];
+  for (var j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+
+  var i = 0;
+  var j = 0;
+  var k = l;
+
+  while (i < n1 && j < n2) {
+    if (L[i] <= R[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = R[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    arr[k] = R[j];
+    j++;
+    k++;
+  }
+  console.log(arr);
+}
+
+console.log(mergeSort([1, 8, 5, 9, 6], 0, 4));
