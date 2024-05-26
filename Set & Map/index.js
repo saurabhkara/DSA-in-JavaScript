7b36d71d2db405c3fb25b564dcc8a544db308354
Saurabh <saurabkumar679@gmail..com>
2024-05-26 06:49:14 +0530

    map and weakmap

--- Set & Map/index.js
+++ Set & Map/index.js
@@ -14,3 +14,37 @@ console.log(map);
 map.forEach((item) => console.log(item));
 
 // Q.2 Sort the characters by freequency
+
+const str = "tree";
+
+function sortCharacterbasedOnOccurance(str) {
+  const map = new Map();
+  for (let char of str) {
+    if (map.has(char)) {
+      map.set(char, (map.get(char) || 0) + 1);
+    } else {
+      map.set(char, 1);
+    }
+  }
+
+  const sortedArr = [...map.keys()].sort((a, b) => map.get(b) - map.get(a));
+  let result = "";
+  for (let ch of sortedArr) {
+    result += ch.repeat(map.get(ch));
+  }
+  return result;
+}
+
+console.log(sortCharacterbasedOnOccurance(str));
+
+// Q.3 Map vs Object
+//Map is faster than object.
+
+// Q.4 Map vs WeekMap
+// A WeakMap is a collection of key/value pairs whose keys must be objects.
+
+const weekmap = new WeakMap();
+weekmap.set({ name: "saurabh" }, "mehjsaj");
+console.log(weekmap);
+
+// Set in javaScript
