b30bf864cc12726e55d7f949a6f894fd6bcee9f0
Saurabh <saurabkumar679@gmail..com>
2024-07-28 14:18:31 +0530

    28-7-2024

--- Linked-List/index.js
+++ Linked-List/index.js
@@ -135,11 +135,12 @@ class LinkedList {
 }
 
 const list = new LinkedList();
-// list.insertAtHead(52);
-// list.insertAtHead(65);
-// list.insertAtHead(98);
+list.insertAtHead(52);
+list.insertAtHead(65);
+list.insertAtHead(98);
+// list.insertAtHead(99);
 // console.log(list.removeFromHead());
-// console.log(list.insertAt(2, 50));
+console.log(list.insertAt(2, 50));
 // list.insertAtEnd(850);
 // list.print();
 // // console.log(list.removeFrom(2));
@@ -152,30 +153,85 @@ const list = new LinkedList();
 // Q.1 Check linkedlist contains cycle
 
 // Q.2 Middle of Likedlist in O(n) using double pointer approach
+LinkedList.prototype.middleNodeUsingFastSLowPointer = function () {
+  let fast = this.head;
+  let slow = this.head;
 
+  while (slow !== null && fast.next !== null) {
+    slow = slow.next;
+    if (fast.next?.next) {
+      fast = fast.next.next;
+    } else {
+      fast = fast.next;
+    }
+  }
+  console.log(slow);
+};
+list.middleNodeUsingFastSLowPointer();
+// console.log(list);
 // Q.3 Reverse a linkedlist
 
-list.insertAtEnd(2);
-list.insertAtEnd(4);
-list.insertAtEnd(3);
-console.log(list);
-const value1 = list.print();
-// console.log(value1);
+// Q.4 Question from leetcode
+// list.insertAtEnd(2);
+// list.insertAtEnd(4);
+// list.insertAtEnd(3);
+// console.log(list);
+// const value1 = list.print();
 
-const list2 = new LinkedList();
-list2.insertAtEnd(5);
-list2.insertAtEnd(6);
-list2.insertAtEnd(4);
-const value2 = list2.print();
-// console.log(value2);
+// const list2 = new LinkedList();
+// list2.insertAtEnd(5);
+// list2.insertAtEnd(6);
+// list2.insertAtEnd(4);
+// const value2 = list2.print();
 
-function addReverse(value1, value2) {
-  let iValue1 = value1.split("").reverse().join("");
-  iValue1 = parseInt(iValue1);
-  value2 = parseInt(value2.split("").reverse().join(""));
-  let result = iValue1 + value2;
-  result = result.toString().split("").reverse().join("");
-  console.log(result);
-  return result;
+// function addReverse(value1, value2) {
+//   let iValue1 = value1.split("").reverse().join("");
+//   iValue1 = parseInt(iValue1);
+//   value2 = parseInt(value2.split("").reverse().join(""));
+//   let result = iValue1 + value2;
+//   result = result.toString().split("").reverse().join("");
+//   console.log(result);
+//   return result;
+// }
+// addReverse(value1, value2);
+
+class DoubleLLNode {
+  constructor(value) {
+    this.prev = null;
+    this.data = value;
+    this.next = null;
+  }
 }
-addReverse(value1, value2);
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  insertAtHead(value) {
+    const node = new DoubleLLNode(value);
+    if (this.head === null) {
+      this.tail = node;
+    }
+    if (this.head !== null) {
+      node.next = this.head;
+      this.head.prev = node;
+    }
+    this.head = node;
+    return this.head;
+  }
+  insertAtEnd(value) {
+    const node = new DoubleLLNode(value);
+    this.tail.next = node;
+    node.prev = this.tail;
+    this.tail = node;
+  }
+}
+
+const doubleLL = new DoublyLinkedList();
+doubleLL.insertAtHead(20);
+doubleLL.insertAtHead(15);
+doubleLL.insertAtHead(10);
+doubleLL.insertAtEnd(52);
+console.log(doubleLL);
---
+++ Tree/index.js
@@ -0,0 +1,1 @@
+// Tree - is a non-linear data structure in which a collection of elements known as nodes are connected to each other via edges such that there exists exactly one path between any two nodes.
