2b2206f2eac06bf5d6d0481f924e50c666832a44
Saurabh <saurabkumar679@gmail..com>
2024-07-28 16:00:00 +0530

    28-07-2024

--- Tree/index.js
+++ Tree/index.js
@@ -1,1 +1,8 @@
 // Tree - is a non-linear data structure in which a collection of elements known as nodes are connected to each other via edges such that there exists exactly one path between any two nodes.
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.data = value;
+  }
+}
