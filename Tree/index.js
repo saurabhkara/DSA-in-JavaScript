241858254c4a5b21dd6e07d85daa0f87ec731eb9
Saurabh <saurabkumar679@gmail..com>
2024-08-03 13:15:02 +0530

    03-08-2024

--- Tree/index.js
+++ Tree/index.js
@@ -6,3 +6,52 @@ class Node {
     this.data = value;
   }
 }
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(key) {
+    const node = new Node(key);
+    if (this.root === null) {
+      this.root = node;
+      return true;
+    }
+
+    let temp = this.root;
+
+    let flag = true;
+    while (flag) {
+      console.log(temp);
+      if (temp.data === key) {
+        flag = false;
+        return false;
+      }
+
+      if (temp.data > key) {
+        if (temp.left === null) {
+          temp.left = node;
+          flag = false;
+          return true;
+        } else {
+          temp = temp.left;
+        }
+      } else if (temp.data < key) {
+        if (temp.right === null) {
+          temp.right = node;
+          flag = false;
+          return true;
+        } else {
+          temp = temp.right;
+        }
+      }
+    }
+  }
+}
+
+const bst = new BST();
+bst.insert(54);
+bst.insert(30);
+bst.insert(55);
+console.log(bst);
